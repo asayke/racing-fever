72f7172795f79876bd2bdda9f1b06343f7bfa9bb
oleja <noilzep@gmail.com>
2023-04-02 18:32:30 +0300

    Added statistics scene

---
+++ Assets/Scripts/Menu/STATISTICS/StatisticsButtons.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StatisticsButtons : MonoBehaviour
+{
+    [SerializeField] private Button _back;
+
+    private void Awake() => _back.onClick.AddListener(Back);
+
+    private void Back() => SceneManager.LoadScene("MainMenu");
+}
